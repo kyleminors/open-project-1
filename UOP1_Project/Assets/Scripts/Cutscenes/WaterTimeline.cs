ad1b256b8fd60b16c93b4376d81323c44be88e18
Kyle Minors <kyleminors@gmail.com>
2021-03-08 12:12:44 -0500

    Water shader updated with Timeline

---
+++ UOP1_Project/Assets/Scripts/Cutscenes/WaterTimeline.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+[ExecuteAlways]
+public class WaterTimeline : MonoBehaviour
+{
+	public PlayableTrack playable;
+
+	public Material ocean;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    ocean.SetFloat("", (float)playable.duration);
+	   // Debug.Log(playable.duration);
+    }
+}
